5c65d260bd54ad2aeff616a085ec00cd1526d7cb
John Burwell <john@b-wells.us>
2023-04-30 21:25:23 -0500

    rename listu to listunread

--- rsbbs/plugins/listunread/plugin.py
+++ rsbbs/plugins/listunread/plugin.py
@@ -32,7 +32,7 @@ class Plugin():
 
     def init_parser(self, parser: Parser) -> None:
         subparser = parser.subparsers.add_parser(
-            name='listu',
+            name='listunread',
             aliases=['lu'],
             help='List unread messages addressed to you')
         subparser.set_defaults(func=self.run)
