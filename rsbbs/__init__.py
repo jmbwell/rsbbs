4eceac78b4e5a90681da3c4e43cb6b58bb8c9d30
John Burwell <john@b-wells.us>
2023-04-30 20:21:31 -0500

    increment version cos seems about time

--- rsbbs/__init__.py
+++ rsbbs/__init__.py
@@ -19,7 +19,7 @@
 __all__ = ['config', 'console', 'controller',
            'logger', 'models', 'parser', 'pluginloader']
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from .config import Config
 from .console import Console
